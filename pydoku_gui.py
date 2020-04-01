a21fbedb402bea71209ce442ca74dc8a932442d4
Matija <matija.hericko@gmail.com>
2020-04-01 19:27:16 +0200

    Adjusted cell sizes, added marking selected cell

--- pydoku_gui.py
+++ pydoku_gui.py
@@ -10,15 +10,19 @@ font = pygame.font.SysFont('Monospace', 32)
 
 class PydokuGUI:
 
-    WIDTH = 9 * 40 + 6 * 1 + 2 * 3
+    # colors that are used
     COLOR_BLACK = (0, 0, 0)
     COLOR_WHITE = (255, 255, 255)
-    cellCenters = [20, 61, 102, 145, 187, 228, 271, 312, 353]
+    COLOR_GREEN = (0, 255, 0)
+    COLOR_LIGHT_BLUE = (0, 180, 255)
+
+    WIDTH = 9 * 39 + 6 * 1 + 2 * 3
+    cellCenters = [20, 60, 100, 142, 182, 222, 264, 304, 344]
 
     def __init__(self):
         self.screen = pygame.display.set_mode((self.WIDTH, self.WIDTH))
         pygame.display.set_caption('Pydoku GUI')
-        self.selectedCell = None
+        self.selectedCellRowAndColumn = None
         self.board = [
             [0, 0, 9, 2, 1, 8, 0, 0, 0],
             [1, 7, 0, 0, 9, 6, 8, 0, 0],
@@ -34,6 +38,7 @@ class PydokuGUI:
     def drawBoard(self):
         self.drawLines()
         self.drawNumbers()
+        self.drawSelected()
 
     def drawLines(self):
         # Fill the background
@@ -42,35 +47,38 @@ class PydokuGUI:
         # Draw the lines for the Sudoku board
         x = 40
         for i in range(1, 10):
-            self.drawLine((x, 0), (x, self.WIDTH))
-            self.drawLine((0, x), (self.WIDTH, x))
+            self.drawLine((x, 0), (x, self.WIDTH), self.COLOR_BLACK)
+            self.drawLine((0, x), (self.WIDTH, x), self.COLOR_BLACK)
 
             # If its the third line we want to make it appear thicker,
             # so we draw three consecutive lines
             if i % 3 == 0:
                 x += 1
-                self.drawLine((x, 0), (x, self.WIDTH))
-                self.drawLine((0, x), (self.WIDTH, x))
+                self.drawLine((x, 0), (x, self.WIDTH), self.COLOR_BLACK)
+                self.drawLine((0, x), (self.WIDTH, x), self.COLOR_BLACK)
                 x += 1
-                self.drawLine((x, 0), (x, self.WIDTH))
-                self.drawLine((0, x), (self.WIDTH, x))
+                self.drawLine((x, 0), (x, self.WIDTH), self.COLOR_BLACK)
+                self.drawLine((0, x), (self.WIDTH, x), self.COLOR_BLACK)
 
-            x += 41
+            x += 40
 
-    def drawLine(self, startPoint, endPoint):
-        pygame.draw.line(self.screen, self.COLOR_BLACK, startPoint, endPoint)
+    def drawLine(self, startPoint, endPoint, color):
+        pygame.draw.line(self.screen, color, startPoint, endPoint)
 
     def drawNumbers(self):
         # Go through the board and draw the numbers into the cells
         for row in range(len(self.board)):
             for column in range(len(self.board[row])):
                 if self.board[row][column] != 0:
-                    self.drawNumber(
-                        self.board[row][column], self.cellCenters[column], self.cellCenters[row])
+                    self.drawNumber(self.board[row][column],
+                                    self.cellCenters[column],
+                                    self.cellCenters[row])
 
     def drawNumber(self, number, centerX, centerY):
-        text = font.render(str(number), True,
-                           self.COLOR_BLACK, self.COLOR_WHITE)
+        text = font.render(str(number),
+                           True,
+                           self.COLOR_BLACK,
+                           self.COLOR_WHITE)
 
         textRect = text.get_rect()
 
@@ -79,11 +87,38 @@ class PydokuGUI:
 
         self.screen.blit(text, textRect)
 
+    def drawSelected(self):
+        if self.selectedCell is None:
+            return
+
+        columnCenter = self.cellCenters[self.selectedCellRowAndColumn[0]]
+        rowCenter = self.cellCenters[self.selectedCellRowAndColumn[1]]
+
+        self.markSquare(columnCenter, rowCenter, self.COLOR_LIGHT_BLUE)
+
+    def markSquare(self, columnCenter, rowCenter, color):
+        self.drawLine((columnCenter - 20, rowCenter - 20),
+                      (columnCenter + 20, rowCenter - 20),
+                      color)
+
+        self.drawLine((columnCenter - 20, rowCenter + 20),
+                      (columnCenter + 20, rowCenter + 20),
+                      color)
+
+        self.drawLine((columnCenter - 20, rowCenter - 20),
+                      (columnCenter - 20, rowCenter + 20),
+                      color)
+
+        self.drawLine((columnCenter + 20, rowCenter - 20),
+                      (columnCenter + 20, rowCenter + 20),
+                      color)
+
     def setSelectedCell(self, mouseClickPosition):
-        row = self.getCellFromCoord(mouseClickPosition[1])
-        column = self.getCellFromCoord(mouseClickPosition[0])
-        if row != -1 and column != -1:
-            self.selectedCell = (column, row)
+        clickedRow = self.getCellFromCoord(mouseClickPosition[1])
+        clickedColumn = self.getCellFromCoord(mouseClickPosition[0])
+
+        if clickedRow >= 0 and clickedColumn >= 0:
+            self.selectedCellRowAndColumn = (clickedColumn, clickedRow)
 
     def getCellFromCoord(self, coordinate):
         for idx, cellCenter in enumerate(self.cellCenters):
